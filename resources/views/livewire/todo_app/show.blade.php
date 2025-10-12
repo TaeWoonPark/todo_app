b10299761c36ff8fa7a4bf891964d0a438ecebe6
Tae Woon Park <yasuo19@gmail.com>
2025-10-12 17:53:12 +0900

    課題1完了

--- resources/views/livewire/todo_app/show.blade.php
+++ resources/views/livewire/todo_app/show.blade.php
@@ -1,12 +1,25 @@
 <?php
 
 use function Livewire\Volt\{state};
+use App\Models\Task;
 
-//
+// ルートモデルバインディング
+state(['task' => fn(Task $task) => $task]);
+$edit = function () {
+    return redirect()->route('todo_app.edit', $this->task);
+};
+
+$destroy = function () {
+    $this->task->delete();
+    return redirect()->route('todo_app.index');
+};
 
 ?>
 
 <div>
-    <h1>タイトル</h1>
-    <p>本文</p>
+    <a href="{{ route('todo_app.index') }}">戻る</a>
+    <h1>{{ $task->title }}</h1>
+    <p>{!! nl2br(e($task->description)) !!}</p>
+    <button wire:click="edit">編集する</button>
+    <button wire:click="destroy" wire:confirm="本当に削除しますか？">削除する</button>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 一覧ページ
+Volt::route('/todo_app', 'todo_app.index')->name('todo_app.index');
+Volt::route('/todo_app/create', 'todo_app.create')->name('todo_app.create');
 // 詳細ページ
-Volt::route('/todo_app/{memo}', 'todo_app.show')->name('todo_app.show');
+Volt::route('/todo_app/{task}', 'todo_app.show')->name('todo_app.show');
+Volt::route('/todo_app/{task}/edit', 'todo_app.edit')->name('todo_app.edit');
+
