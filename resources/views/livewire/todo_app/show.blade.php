01b326aa246bc493fdc9c435736b609022aab484
Tae Woon Park <yasuo19@gmail.com>
2025-10-12 15:14:55 +0900

    first commit

---
+++ resources/views/livewire/todo_app/show.blade.php
@@ -0,0 +1,12 @@
+<?php
+
+use function Livewire\Volt\{state};
+
+//
+
+?>
+
+<div>
+    <h1>タイトル</h1>
+    <p>本文</p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 詳細ページ
+Volt::route('/todo_app/{memo}', 'todo_app.show')->name('todo_app.show');
